16f907583a88652e2b40ac0ea5e9763cf0792c86
rebeccanoel <rnoel@mit.edu>
2017-07-27 16:57:34 -0400

    Add files via upload

--- LeNetAdaptedSand.py
+++ LeNetAdaptedSand.py
@@ -51,7 +51,7 @@ input_size = image_size**2
 num_classes = num_labels
 state_size = 2000
 num_batches = 2000
-num_steps = 2
+num_steps = 5
 
 import numpy as np
 
@@ -61,41 +61,17 @@ def reformat(dataset, labels):
   labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
   return dataset, labels
 
-
-#the following created to unstack matricies in the case where num_steps is included in the tensor shape itsself
-tr_data, train_labels = reformat(X_train, y_train)
-v_data, valid_labels = reformat(X_validation, y_validation)
-te_data , test_labels  = reformat(X_test, y_test)
-submission_dataset = submission_dataset.as_matrix().reshape((-1, image_size, image_size, num_channels)).astype(np.float32)
-#Unstacking in order to incorporate num_steps parameter in above
-train_dataset = tf.unstack(tr_data, num = num_steps, axis = 1)
-valid_dataset = tf.unstack(v_data, num = num_steps, axis = 1)
-test_dataset = tf.unstack(te_data, num = num_steps, axis = 1)
-print ('Training set   :', len(train_dataset), len(train_labels))
-
-def gen_timestep_matrix(data, num_steps)
-    expanded_tensor = tf.expand_dims(data)
-    expand_2 = expanded_tensor
-    for n in range(num_steps-1):
-        expand_2 = tf.stack(expanded_tensor, expand_2)
-    return expand_2
-
-'''
-    #Original Input Data Format from Le Net
-    tf_train_dataset   = tf.placeholder(tf.float32,shape=(batch_size,image_size,image_size,num_channels))
-    tf_train_labels    = tf.placeholder(tf.float32,shape=(batch_size,num_labels))
-    #validation data 
-    tf_valid_dataset   = tf.constant(valid_dataset)
-    #test data
-    tf_test_dataset    = tf.constant(test_dataset)
-    #submission data
-    tf_submission_data = tf.placeholder(tf.float32,shape=(28000,image_size,image_size,num_channels))
-
 train_dataset, train_labels = reformat(X_train, y_train)
 valid_dataset, valid_labels = reformat(X_validation, y_validation)
 test_dataset , test_labels  = reformat(X_test, y_test)
 submission_dataset = submission_dataset.as_matrix().reshape((-1, image_size, image_size, num_channels)).astype(np.float32)
-'''
+
+
+def gen_timestep_matrix(data, num_steps):
+    expanded_tensor = np.tile(data,num_steps)
+    return expanded_tensor
+
+#Original Input Data Format from Le Net
 
 print ('Validation set :', len(valid_dataset), len(valid_labels))
 print ('Test set       :', len(test_dataset), len(test_labels))
@@ -175,55 +151,77 @@ finalImageSize = output_size_pool(input_size=image_size, conv_filter_size=kernel
                                   pool_filter_size=poolFilterSize, padding=padding,
                                   conv_stride=convStride, pool_stride=poolStride)
 
-def weightBuilder(shape,name):
-    #shape = [patchSize,patchSize,channel,depth]
-        return tf.Variable(tf.truncated_normal(shape, stddev=0.01),name=name)
 
-def biasesBuilder(shape,name):
-    #shape = depth  size
-    return tf.Variable(tf.constant(1.0, shape=shape),name=name)
 
-def conv2d(x,W,name):
-    return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding=padding,name=name)
+'''
+removing def of weight builder etc. from here
+'''
 
-def maxPool_2x2(x,name):
-    return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding=padding,name=name)
 
-def rnn_cell(rnn_input, W, b, state):
-    print("Trying to concat rnn_input + state: ", rnn_input, " + ", state)
-    print("Trying to multiply concat * W: ", tf.concat([rnn_input, state], 1), " * ", W)
-    print("Trying to add: matmul + b", tf.matmul(tf.concat([rnn_input, state], 1), W), " + ", b)
-    return tf.nn.relu(tf.matmul(tf.concat([rnn_input, state], 1), W) + b)
-
-C1_w = weightBuilder([kernelSize,kernelSize,1,depth1Size],"C1_w")
-C1_b = biasesBuilder([depth1Size],"C1_b")
-C2_w = weightBuilder([kernelSize,kernelSize,depth1Size,depth2Size],"C2_w")
-C2_b = biasesBuilder([depth2Size],"C2_b")
-FC1_w = weightBuilder([finalImageSize*finalImageSize*depth2Size,FC1HiddenUnit],"FC1_w")
-FC1_b = biasesBuilder([FC1HiddenUnit],"FC1_b")
-keep_prob = tf.placeholder(dtype=tf.float32,name="keepProb")
-FC2_w = weightBuilder([FC1HiddenUnit,FC2HiddenUnit],"FC2_w")
-FC2_b = biasesBuilder([FC2HiddenUnit],"FC2_b")
-FC3_w = weightBuilder([state_size,num_labels],"FC3_w")
-FC3_b = biasesBuilder([num_labels],"FC3_b")
-RNN_w = weightBuilder([state_size + input_size, state_size], name = "RNN_Weights")
-RNN_b = biasesBuilder([state_size], name = "RNN_Biases")
-tf_train_dataset   = tf.placeholder(tf.float32,shape=(num_steps, batch_size,image_size,image_size,num_channels))
-tf_train_labels    = tf.placeholder(tf.float32,shape=(num_steps, batch_size,num_labels))
-init_state_train = tf.zeros([batch_size,state_size])
-tf_submission_data = tf.placeholder(tf.float32,shape=(num_steps, 28000,image_size,image_size,num_channels))
-init_state_sub = tf.zeros([28000,state_size])
 
 graph = tf.Graph()
 with graph.as_default():
+
+    def weightBuilder(shape,name):
+    #shape = [patchSize,patchSize,channel,depth]
+        return tf.Variable(tf.truncated_normal(shape, stddev=0.01),name=name)
+
+    def biasesBuilder(shape,name):
+        #shape = depth  size
+        return tf.Variable(tf.constant(1.0, shape=shape),name=name)
+
+    def conv2d(x,W,name):
+        return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding=padding,name=name)
+
+    def maxPool_2x2(x,name):
+        return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding=padding,name=name)
+
+    def rnn_cell(rnn_input, W, b, state):
+        return tf.nn.relu(tf.matmul(tf.concat([rnn_input, state], 1), W) + b)
+
+    C1_w = weightBuilder([kernelSize,kernelSize,1,depth1Size],"C1_w")
+    C1_b = biasesBuilder([depth1Size],"C1_b")
+    C2_w = weightBuilder([kernelSize,kernelSize,depth1Size,depth2Size],"C2_w")
+    C2_b = biasesBuilder([depth2Size],"C2_b")
+    FC1_w = weightBuilder([finalImageSize*finalImageSize*depth2Size,FC1HiddenUnit],"FC1_w")
+    FC1_b = biasesBuilder([FC1HiddenUnit],"FC1_b")
+    keep_prob = tf.placeholder(dtype=tf.float32,name="keepProb")
+    FC2_w = weightBuilder([FC1HiddenUnit,FC2HiddenUnit],"FC2_w")
+    FC2_b = biasesBuilder([FC2HiddenUnit],"FC2_b")
+    FC3_w = weightBuilder([state_size,num_labels],"FC3_w")
+    FC3_b = biasesBuilder([num_labels],"FC3_b")
+    RNN_w = weightBuilder([state_size + input_size, state_size], name = "RNN_Weights")
+    RNN_b = biasesBuilder([state_size], name = "RNN_Biases")
+
+
+    #tf_train_dataset   = tf.placeholder(tf.float32,shape=(num_steps, batch_size,image_size,image_size,num_channels))
+    #tf_train_labels    = tf.placeholder(tf.float32,shape=(num_steps, batch_size,num_labels))
+    init_state_train = tf.zeros([batch_size,state_size])
+    init_state_valid = tf.zeros([batch_size,state_size])
+    tf_submission_data = tf.placeholder(tf.float32,shape=(num_steps, 28000,image_size,image_size,num_channels))
+    init_state_sub = tf.zeros([28000,state_size])
+
+
+    tf_train_dataset   = tf.placeholder(tf.float32,shape=(batch_size,image_size,image_size,num_channels,num_steps))
+    tf_train_labels    = tf.placeholder(tf.float32,shape=(batch_size,num_labels,num_steps))
+    #validation data 
+    tf_valid_dataset   = tf.constant(gen_timestep_matrix(valid_dataset,num_steps))
+    #test data
+    tf_test_dataset    = tf.constant(gen_timestep_matrix(test_dataset,num_steps))
+    #submission data
+    tf_submission_data = tf.placeholder(tf.float32,shape=(28000,image_size,image_size,num_channels,num_steps))
+
     def leNet5(data,state_in):
         #C1
+        print("///// data is: ", type(data),data, "c1_w is: ", type(C1_w),C1_w, "c1_b is: ", type(C1_b),C1_b)
         h_conv = tf.nn.relu(conv2d(data,C1_w,"conv1")+C1_b)
+        
         #S2
         h_pool = maxPool_2x2(h_conv,"pool1")
        
         #C3 
         h_conv = tf.nn.relu(conv2d(h_pool,C2_w,"conv2")+C2_b)
+        
         #S4
         h_pool = maxPool_2x2(h_conv,"pool2")
         
@@ -240,52 +238,41 @@ with graph.as_default():
         #F6
         h_FC2 = tf.nn.relu(tf.matmul(h_FC1,FC2_w)+FC2_b)
         h_FC2 = tf.nn.dropout(h_FC2,keep_prob=keep_prob)
-        print(h_FC2)
-        '''
-        print("state_in is: ", state_in)
-
-        state_in_shape = state_in.get_shape().as_list()
-        number_batches = state_in_shape[0]/batch_size
-        print("num_batches is: ", number_batches)
-
-        state_in_split = tf.split(state_in, int(number_batches))
         
-        print("this is split state_in:", state_in_split)
-    
-        '''
-        #h_FC2 --> (16,784)
-        #state --> (784,2000) (can't concat)
-        #for batch in state_in_split:
-            #print("batch: ", batch, "state_in_spl")
-            #state_out = rnn_cell(h_FC2, RNN_w, RNN_b, state_in)
+        #RNN
         state_out = rnn_cell(h_FC2, RNN_w, RNN_b, state_in)
 
         #FC3
         FC_output = tf.matmul(state_out,FC3_w)+FC3_b
-        print("This is FC_Output: ", FC_output)
         #shape is (16, 10)
 
-        #RNN
-        print("THIS IS RNN_INPUT", FC_output)
-        
         return state_out, FC_output #rnn_cell(rnn_in, RNN_w, RNN_b, state)
     
 
-    tt=tf.unstack(tf_train_dataset)
+    tt=tf.unstack(tf_train_dataset, axis = -1)
     state_out = init_state_train
-    print("this is train_dataset: ", train_dataset)
+    #print("this is tt: ", tt)
+    #print("this is train_dataset: ", train_dataset)
     for train in tt:
-        print("!!!!! This is train: ", train)
+        #print("!!!!! This is train: ", train)
         state_out, logits = leNet5(train, state_out)
-    state_out = init_state_valid
-    for valid in valid_dataset:
+    #state_out = init_state_valid
+    
+    tv = tf.unstack(tf_valid_dataset, axis = -1)
+    print("This is tv before and after unstack: ", tf_valid_dataset, tv)
+
+    for valid in tv:
+        print("@@@ this is data passed into lenet: ", valid)
         state_out, logits = leNet5(valid, state_out)
-    state_out = init_state_test
-    for test in test_dataset:
+    #state_out = init_state_test
+
+    tte = tf.unstack(tf_test_dataset)
+    for test in tte:
         state_out, logits = leNet5(test, state_out)
 
+    ts = tf.unstack(tf_submission_data, axis = -1)
     state_out = init_state_sub
-    for sub in submission_data:
+    for sub in ts:
         state_out, logits = leNet(sub, state_out)
 
     with tf.name_scope('convolution1') as scope:
